338060844adddf1a2e09a6fdc9fe3d7c296a161b
JANG-GWANG <103880940+JANG-GWANG@users.noreply.github.com>
2022-07-07 17:51:43 +0900

    Update java.js

--- java.js
+++ java.js
@@ -1,34 +1,30 @@
-
-// 카카오 지도 api
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-    var options = { //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(35.871691843254716, 128.62496478375644), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-    };
-
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴			
-
-    // 마커가 표시될 위치입니다 
-    var markerPosition = new kakao.maps.LatLng(35.871691843254716, 128.62496478375644);
-
-    var imageSrc = './resoft_image/지도마커.png', // 마커이미지의 주소입니다    
-        imageSize = new kakao.maps.Size(220, 45), // 마커이미지의 크기입니다
-        imageOption = {
-            offset: new kakao.maps.Point(27, 69)
-        }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-
-    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
-    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
-        markerPosition = new kakao.maps.LatLng(35.871691843254716, 128.62496478375644); // 마커가 표시될 위치입니다
-            
-    // 마커를 생성합니다
-    var marker = new kakao.maps.Marker({
-        position: markerPosition,
-        image: markerImage
-    });			
-    // 마커가 지도 위에 표시되도록 설정합니다
-    marker.setMap(map);
-    //애니메이션 -> wow로 대체
+//slcik slider
+$( 'document' ).ready( function() {
+    $( '.proj-box-wrapper' ).slick( {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [
+                { breakpoint: 940,
+                    settings:{
+                        slidesToShow:3
+                    }
+                },
+                { breakpoint: 680,
+                    settings:{
+                        slidesToShow:2
+                    }
+                },
+                { breakpoint: 430,
+                    settings:{
+                        slidesToShow:1
+                    }
+                }
+              ]
+    } );
+} );
 
 //  window.matchMedia()를 이용해서 접속하는 웹브라우저의 가로 크기에 따라 다른 작업을 할 수 있습니다. 반응형 웹디자인을 할 때 유용한 메소드
 // 가로가 1217px라면 customer_list를 숨깁니다. hide()
@@ -164,30 +160,33 @@ moveBtn.addEventListener('click', function() {
 }
 })
 
+// 카카오 지도 api
+var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+    var options = { //지도를 생성할 때 필요한 기본 옵션
+        center: new kakao.maps.LatLng(35.871691843254716, 128.62496478375644), //지도의 중심좌표.
+        level: 3 //지도의 레벨(확대, 축소 정도)
+    };
 
-$( 'document' ).ready( function() {
-    $( '.proj-box-wrapper' ).slick( {
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    responsive: [
-                { breakpoint: 940,
-                    settings:{
-                        slidesToShow:3
-                    }
-                },
-                { breakpoint: 680,
-                    settings:{
-                        slidesToShow:2
-                    }
-                },
-                { breakpoint: 430,
-                    settings:{
-                        slidesToShow:1
-                    }
-                }
-              ]
-    } );
-} );
+    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴			
+
+    // 마커가 표시될 위치입니다 
+    var markerPosition = new kakao.maps.LatLng(35.871691843254716, 128.62496478375644);
+
+    var imageSrc = './resoft_image/지도마커.png', // 마커이미지의 주소입니다    
+        imageSize = new kakao.maps.Size(220, 45), // 마커이미지의 크기입니다
+        imageOption = {
+            offset: new kakao.maps.Point(27, 69)
+        }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+        markerPosition = new kakao.maps.LatLng(35.871691843254716, 128.62496478375644); // 마커가 표시될 위치입니다
+            
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition,
+        image: markerImage
+    });			
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+    //애니메이션 -> wow로 대체
